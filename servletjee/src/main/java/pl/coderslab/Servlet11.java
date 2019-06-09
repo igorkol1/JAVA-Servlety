8ccfa34fa6570e8486be1bc5871ca31bad03b013
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-06-09 11:00:16 +0200

    Rozwiązanie zadań z dzień 2 sekcja b

---
+++ servletjee/src/main/java/pl/coderslab/Servlet11.java
@@ -0,0 +1,21 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "Servlet11",urlPatterns={"/servlet11"})
+public class Servlet11 extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Get request in Servlet11");
+        response.getWriter().append("Hello world.");
+    }
+}
---
+++ servletjee/src/main/java/pl/coderslab/Servlet14.java
@@ -0,0 +1,22 @@
+package pl.coderslab;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+@WebServlet(name = "Servlet14",urlPatterns = "/servlet14")
+public class Servlet14 extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Request from ip: "+request.getRemoteAddr()+"\n");
+        response.getWriter().append("Request from browser: "+request.getHeader("User-Agent")+"\n");
+        response.getWriter().append("Current date: "+LocalDateTime.now().toString());
+    }
+}
