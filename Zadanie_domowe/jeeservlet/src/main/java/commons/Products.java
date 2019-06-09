dc19355f23868903bc2d996e70fadf16cf803345
Igor Kołodziejczyk <igor.k.kolodziejczyk@gmail.com>
2019-06-09 15:07:13 +0200

    Rozwiązanie zadań domowych z dzień 2

---
+++ Zadanie_domowe/jeeservlet/src/main/java/commons/Products.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Products {
+
+    private String[] products = {
+            "Asus Transformr;2999.99",
+            "iPhone 6';3499.18",
+            "Converse Sneakers;125.00",
+            "LG OLED55B6P OLED TV;6493.91",
+            "Samsung HT-J4100;800.99",
+            "Alpine Swiss Dress Belt;99.08",
+            "60 Watt LED;1.50",
+            "Arduino Nano;3.26",
+    };
+
+    
+
+
+}
